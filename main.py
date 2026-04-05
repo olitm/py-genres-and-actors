85097bd6a8bcbbfef6107d78d5c1dd14b6dac0a9
Ol. <holmate@dontsp.am>
2026-04-05 22:20:32 +0300

    fixed a problem with updating the record

--- main.py
+++ main.py
@@ -31,11 +31,19 @@ def main() -> QuerySet:
             last_name=last_name,
         )
 
-    Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu").update(
-        first_name="Keanu", last_name="Reeves",
-    )
+    Genre.objects.filter(
+        name="Dramma").update(
+        name="Drama")
+
+    Actor.objects.filter(
+        first_name="George",
+        last_name="Klooney").update(
+        last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu", last_name="Reeves")
+
     Genre.objects.filter(name="Action", ).delete()
     Actor.objects.filter(first_name="Scarlett", ).delete()
+
     return Actor.objects.filter(last_name="Smith", ).order_by("first_name")
