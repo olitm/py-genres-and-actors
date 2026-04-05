b1fb011bb5467e5ace2d8d0df8ce74ca23d1b7d7
Ol. <holmate@dontsp.am>
2026-04-05 22:12:46 +0300

    fix problem

--- main.py
+++ main.py
@@ -39,7 +39,3 @@ def main() -> QuerySet:
     Genre.objects.filter(name="Action", ).delete()
     Actor.objects.filter(first_name="Scarlett", ).delete()
     return Actor.objects.filter(last_name="Smith", ).order_by("first_name")
-
-
-if __name__ == "__main__":
-    print(main())
